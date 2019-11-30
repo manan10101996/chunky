d39201d4807acf6a002ed54ce3167c77e12adc81
Manan Shah <manan.shah@easebuzz.in>
2019-11-30 15:26:41 +0530

    work in progress...

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Author: Manan Shah
 
+var currencies = [
+    { id: 1, country: "India", notation: "Rs."}
+]
+
 class Chunky {
     // Constructor.
     constructor() {
@@ -13,8 +17,8 @@ class Chunky {
         this.sessionLogCount += 1;
     }
 
-    // Log seperator. (0 = Thin (Default), 1 = Medium, 2 = Fat)
-    shootSeperator(type=0) {
+    // Log a seperator. (type: 0 = Thin (Default), 1 = Medium, 2 = Fat)
+    seperator(type=0) {
         var logText = "--------------------------------------------------";
         switch(type) {
             case 0:
@@ -31,4 +35,44 @@ class Chunky {
         console.log(logText);
         this.postLog();
     }
+
+    // Log a message. (message: String, type: info, warning, danger)
+    message(message="", type="info") {
+        switch(type) {
+            case "info":
+                console.info(message);
+                break;
+            case "warning":
+                console.warn(message);
+                break;
+            case "danger":
+                console.error(message);
+                break;
+            default:
+                console.log(message);
+        }
+        this.postLog();
+    }
+
+    // Log a colorful message.
+    color(message="", foreground="black", background="white") {
+        console.log(`%c${message}`, `background: ${background}; color: ${foreground}`);
+        this.postLog();
+    }
+
+    // Log a country's currency amount.
+    currency(country="USA", amount=0.00) {
+        var x = currencies.find((currency) => currency.country == "India");
+        var notation = x && x.notation || "--";
+        console.log(`${notation} ${amount}`);
+        this.postLog();
+    }
+
+    // Log an array's objects in tabular form.
+    table() {
+        console.table(currencies);
+        this.postLog();
+    }
 }
+
+window.Chunky = Chunky;
